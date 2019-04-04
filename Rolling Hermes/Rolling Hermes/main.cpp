612bd58d4b67f627c39023625eab580beadc23d9
Jack Zhao <jack_zhao98@outlook.com>
2019-04-03 19:53:14 -0700

    start

---
+++ Rolling Hermes/Rolling Hermes/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Rolling Hermes
+//
+//  Created by Winnie Jeng, Jack Zhao on 4/3/19.
+//  Copyright © 2019 Winnie Jeng, Jack Zhao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    
+    
+    return 0;
+}
