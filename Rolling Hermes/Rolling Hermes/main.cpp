77d5468925e8e882457ada9d996ca10d0abeeed0
Jack Zhao <jack_zhao98@outlook.com>
2019-04-21 21:46:01 -0700

    Tower of Hanoi 1.1

--- Rolling Hermes/Rolling Hermes/main.cpp
+++ Rolling Hermes/Rolling Hermes/main.cpp
@@ -19,23 +19,22 @@ void testSB();
 
 int main(int argc, const char * argv[]) {
 
-//    UIDelegate ui(7);
-//    ui.build();
-//    
-    board a(6);
-    auto start_PQ = chrono::high_resolution_clock::now();
-    a.autoMove(true);
-    auto stop_PQ = chrono::high_resolution_clock::now();
-    auto PQDuration = std::chrono::duration_cast<chrono::microseconds>(stop_PQ - start_PQ);
-
-
-    auto start_ST = chrono::high_resolution_clock::now();
-    testSB();
-    auto stop_ST = chrono::high_resolution_clock::now();
-    auto STDuration = std::chrono::duration_cast<chrono::microseconds>(stop_ST - start_ST);
-
-    cout  << "Priority Queue took " << PQDuration.count()  << "ms. " << endl;
-    cout  << "Stack took " << STDuration.count()  << "ms. " << endl;
+    UIDelegate ui(8);
+    ui.build();
+//
+//    auto start_PQ = chrono::high_resolution_clock::now();
+////    a.autoMove(true);
+//    auto stop_PQ = chrono::high_resolution_clock::now();
+//    auto PQDuration = std::chrono::duration_cast<chrono::microseconds>(stop_PQ - start_PQ);
+//
+////
+//    auto start_ST = chrono::high_resolution_clock::now();
+//    testSB();
+//    auto stop_ST = chrono::high_resolution_clock::now();
+//    auto STDuration = std::chrono::duration_cast<chrono::microseconds>(stop_ST - start_ST);
+//
+//    cout  << "Priority Queue took " << PQDuration.count()  << "ms. " << endl;
+//    cout  << "Stack took " << STDuration.count()  << "ms. " << endl;
 
     
 }
@@ -89,7 +88,7 @@ int main(int argc, const char * argv[]) {
  */
 
 void testSB() {
-    stackBoard(6);
+    stackBoard(10);
 }
 
 void testPQS() {
